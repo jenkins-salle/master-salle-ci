fa196f2d79bce097e8fa42ee3474b831759ad4b4
jenkins-salle <60034053+jenkins-salle@users.noreply.github.com>
2020-01-30 18:41:04 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,6 @@
 
 set -ex
 
-#comentado porque no hace falta levantar la app
-docker-compose up -d quarkus-app
-
 docker-compose run quarkus-build ./mvnw test
 
 docker-compose run quarkus-build ./mvnw verify
